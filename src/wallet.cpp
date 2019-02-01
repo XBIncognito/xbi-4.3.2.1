36c31a24e31d843dce3bb37832187996f30ee824
sub307 <39446798+sub307@users.noreply.github.com>
2019-02-01 18:31:04 +0100

    use references instead of copies in for loops

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -4201,7 +4201,7 @@ bool CWallet::CreateZerocoinSpendTransaction(CAmount nValue, int nSecurityLevel,
         vSelectedMints = SelectMintsFromList(nValueToSelect, nValueSelected, nMaxSpends, fMinimizeChange,
                                              nCoinsReturned, listMints, DenomMap, nNeededSpends);
     } else {
-        for (const CZerocoinMint mint : vSelectedMints)
+        for (const CZerocoinMint& mint : vSelectedMints)
             nValueSelected += ZerocoinDenominationToAmount(mint.GetDenomination());
     }
 
